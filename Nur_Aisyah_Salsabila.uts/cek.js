c4eaebee648b998b577e1c5eef28c622425ffa97
nuraisyahsalsabila <144698603+nuraisyahsalsabila@users.noreply.github.com>
2023-11-19 21:58:02 +0700

    Add files via upload

--- Nur_Aisyah_Salsabila.uts/cek.js
+++ Nur_Aisyah_Salsabila.uts/cek.js
@@ -1,10 +1,15 @@
-var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
-var menu = document.getElementsByClassName('menu')[0];
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
 
-tombolMenu.onclick = function() {
-    menu.classList.toggle('active');
-}
+var tombolMenu = document.getElementsByClassName("tombol-menu")[0];
+var menu = document.getElementsByClassName("menu")[0];
 
-menu.onclick = function() {
-    menu.classList.toggle('active');
-}
+tombolMenu.onclick = function () {
+  menu.classList.toggle("active");
+};
+
+menu.onclick = function () {};
